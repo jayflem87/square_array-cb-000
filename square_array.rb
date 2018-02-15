8051d75dde56f2bee67ea33d348a69e24c41e312
Jay Fleming <jacobmfleming@gmail.com>
2018-02-15 20:47:49 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  array.collect {|x| x * x}
+  squared_array = []
+  array.each do |number|
+    sq_number = number * number
+    squared_array.push(sq_number)
+  end
+  return squared_array
 end
+
+
+
